11533b6ec2ea87703d39b79fe2951b01d9befd84
Vlad V <vladislav.viderko@rubygarage.org>
2019-02-27 17:42:04 +0200

    Configure Knock gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  scope '/api' do
-    scope '/v1' do
+  namespace 'api' do
+    namespace 'v1' do
       resource :user_token, only: :create
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create(
   email: 'vlad@test.io',
-  password: 'qwerty123!',
-  password_confirmation: 'qwerty123!',
+  password: 'qwerty123!A',
+  password_confirmation: 'qwerty123!A',
 );
